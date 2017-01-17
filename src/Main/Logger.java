4319b91ed6db0ca9fdff54b84351f1966207a8e1
Moose <antoine_moghaddar@hotmail.com>
2017-01-17 16:02:13 +0100

    Improvements

--- src/Main/Logger.java
+++ src/Main/Logger.java
@@ -1,8 +1,13 @@
 package Main;
 
+import java.util.Arrays;
+
+import static Main.Logger.Type.*;
+
+
 /**
  * @author Moose (Antoine Moghaddar 8-1-2016.)
- *         Represents messaging system to the console
+ * Represents messaging system to the console
  */
 public class Logger {
 
@@ -16,6 +21,14 @@ public class Logger {
     private static final String ANSI_PURPLE = "\u001B[35m";
     private static final String ANSI_CYAN = "\u001B[36m";
 
+    public static final Type[] LOG_ENABLED = new Type[]{
+            DEFAULT, WARNING, ERROR, NOTICE, CONSOLE, CONFIRMATION, QUESTION, BOARD, DEBUG
+    };
+
+
+    /**
+     * Enum consisting of all logger types
+     */
     @SuppressWarnings("unused")
     public enum Type {
         DEFAULT(ANSI_BLUE),
@@ -29,7 +42,13 @@ public class Logger {
         DEBUG(ANSI_GREEN);
 
         private String color;
+        private int severity;
 
+
+        /**
+         * Constructor // Enum
+         * @param color; Receives ANSI color code
+         */
         Type(String color) {
             this.color = color;
         }
@@ -41,7 +60,6 @@ public class Logger {
 
     /**
      * Prints a message to the console appended with a newline
-     *
      * @param o message being transmitted
      */
     @SuppressWarnings("unused")
@@ -51,7 +69,6 @@ public class Logger {
 
     /**
      * Prints a message, that is formatted, to the console
-     *
      * @param o message being transmitted
      */
     @SuppressWarnings("unused")
@@ -61,15 +78,44 @@ public class Logger {
 
     /**
      * Prints a message to the console
-     *
      * @param o message being transmitted
      */
     public static void print(Logger.Type type, Object o) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(type);
-        stringBuilder.append(" ");
-        stringBuilder.append(o);
+        if(Arrays.stream(LOG_ENABLED).filter(t -> t.equals(type)).count() > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(type);
+            stringBuilder.append(" ");
+            stringBuilder.append(o);
 
-        System.out.print(stringBuilder);
+            System.out.print(stringBuilder);
+        }
+    }
+
+    public static void log(Object o) {
+        println(DEFAULT, o);
+    }
+    public static void warn(Object o) {
+        println(WARNING, o);
+    }
+    public static void err(Object o) {
+        println(ERROR, o);
+    }
+    public static void notice(Object o) {
+        println(Type.NOTICE, o);
+    }
+    public static void console(Object o) {
+        println(Type.CONSOLE, o);
+    }
+    public static void confirm(Object o) {
+        println(Type.CONFIRMATION, o);
+    }
+    public static void question(Object o) {
+        println(Type.QUESTION, o);
+    }
+    public static void board(Object o) {
+        println(Type.BOARD, o);
+    }
+    public static void debug(Object o) {
+        println(Type.DEBUG, o);
     }
 }
